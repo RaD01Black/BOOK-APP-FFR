a5d6e8b7292fae6ec6bb24578a6a2cd6be3b1373
Alereza_RaDBlaCK01 <aliireza01ahmadi@gmail.com>
2024-04-01 20:49:11 +0200

    adding styles

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -1,8 +1,9 @@
 import { FaSearchengin } from "react-icons/fa";
+import styles from "./SeartchBox.module.css"
 
 function SearchBox({ search , setSearch, searchHandler}) {
   return (
-    <div>
+    <div className={styles.search}>
         <input type="text" placeholder="Search title" value={search} onChange={(e) => setSearch(e.target.value.toLowerCase().trim())} />
         <button onClick={searchHandler}><FaSearchengin /></button>
     </div>
